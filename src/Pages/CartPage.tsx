883307679a75e9ad1e060befedb2da4813f87279
prasadyalla2002 <149290187+prasadyalla2002@users.noreply.github.com>
2024-10-07 15:45:42 +0530

    updated from cartbranch

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -1,20 +1,23 @@
+
 import React from "react";
 import CartItem from "../components/CartItem";
 
 const CartPage: React.FC = () => {
   const cartItems = [
-    { name: "Laptop", quantity: 1 },
-    { name: "Smartphone", quantity: 2 },
+    { title: "Laptop", price:60000,quantity:1},
+    { title: "Smartphone",price:35000, quantity: 2 },
+    { title: "earphones",price:5000, quantity: 2 },
   ];
 
   return (
-    <div>
+    <div className="cart-container">
       <h2>Your Cart</h2>
-      {cartItems.map((item) => (
-        <CartItem key={item.name} name={item.name} quantity={item.quantity} />
+      {cartItems.map((item,index) => (
+        <CartItem key={index} title={item.title} price = {item.price} quantity = {item.quantity}/>
       ))}
     </div>
   );
 };
 
 export default CartPage;
+
