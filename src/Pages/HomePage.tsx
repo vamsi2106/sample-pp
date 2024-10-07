96990a8e11618c25869a4d0d73a7c9fc7f5e9648
Lalitendra Swamy Konna <lskonna@gmail.com>
2024-10-07 14:37:24 +0530

    added a test branch for testing

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to Our E-Commerce Website!</h1>
+      <h1>Welcome to Our E-Commerce Website! Testing merge access</h1>
       <p>Find the best products here.</p>
     </div>
   );
