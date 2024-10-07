633e8e3f40bff490089b63bd1961dbd8926d3d0a
prasadyalla2002 <149290187+prasadyalla2002@users.noreply.github.com>
2024-10-07 15:17:29 +0530

    updated cart to work with sample data

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 interface CartItemProps {
-  name: string;
+  title: string;
   quantity: number;
+  price:number
 }
 
-const CartItem: React.FC<CartItemProps> = ({ name, quantity }) => {
+const CartItem: React.FC<CartItemProps> = ({ title, quantity, price }) => {
+
+  const decreaseQuantity = () =>{
+
+  }
+
+  const increaseQuantity = () =>{
+
+  }
+
   return (
     <div className="cart-item">
-      <h4>{name}</h4>
-      <p>Quantity: {quantity}</p>
+      <h4 className="cart-itme-title">{title}</h4>
+     <div className="quantity-container">
+        <button onClick={decreaseQuantity}>-</button>
+        <p>Quantity: {quantity}</p>
+        <button onClick={increaseQuantity}>+</button>
+     </div>
+     <div className="cart-item-price">
+        <h2>{price}</h2>
+     </div>
     </div>
   );
 };
